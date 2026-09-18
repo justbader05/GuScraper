5bfc0db0016e7f5dc662a0daea586bb22654d66e
Bader Al Refai <badermajidalrefai@gmail.com>
2026-09-19 02:33:20 +0300

    Added foreign key enabling to sqlite

--- database/engine.py
+++ database/engine.py
@@ -1,8 +1,14 @@
 from pathlib import Path
-
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, event
+import sqlite3
 
 
 DATABASE_URL = f"sqlite:///{Path(__file__).resolve().parent / 'database.db'}"
 engine = create_engine(DATABASE_URL)
    
+@event.listens_for(engine, "connect")
+def enable_sqlite_foreign_keys(dbapi_connection, connection_record):
+    if isinstance(dbapi_connection, sqlite3.Connection):
+        cursor = dbapi_connection.cursor()
+        cursor.execute("PRAGMA foreign_keys=ON")
+        cursor.close()
