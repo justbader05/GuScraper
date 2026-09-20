db7c118e52e5bf34bed0e2583f76f0656d578af4
Bader Al Refai <badermajidalrefai@gmail.com>
2026-09-20 05:50:22 +0300

    Added automatic file and document type detection

--- classes/material.py
+++ classes/material.py
@@ -1,6 +1,8 @@
 from __future__ import annotations
 from typing import TYPE_CHECKING
 
+from pathlib import Path
+
 from sqlalchemy import String, Integer, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
@@ -15,6 +17,7 @@ class Material(Base):
     moodle_id: Mapped[String] = mapped_column(String(20))
     portal_url: Mapped[str] = mapped_column(String(20), unique=True)
     name: Mapped[str] = mapped_column(String(225))
+    file_path: Mapped[str] = mapped_column(String)
     file_type: Mapped[str] = mapped_column(String(30))
     document_type: Mapped[str] = mapped_column(String(50))
     course_id: Mapped[int] = mapped_column(ForeignKey("Courses.id", ondelete="CASCADE"))
@@ -23,8 +26,28 @@ class Material(Base):
         back_populates="material"
     )
 
-    def __init__(self, moodle_id, portal_url, name, course_id):
+    def detect_file_type(self):
+        return Path(self.file_path).suffix.lstrip(".").lower()
+
+    def detect_document_type(self):
+        split_name = [word.lower() for word in self.name.split()]
+        if self.file_type in ("pptx", "ppt"):
+            return "Chapter"
+
+        elif self.file_type in ("docx", "pdf"):
+            if "syllabus" in split_name:
+                return "Syllabus"
+            else:
+                return "Notes"
+
+        else:
+            return "Other"
+
+    def __init__(self, moodle_id, portal_url, file_path, name, course_id):
         self.moodle_id = moodle_id
         self.portal_url = portal_url
+        self.file_path = file_path
         self.name = name
         self.course_id = course_id
+        self.file_type = self.detect_file_type()
+        self.document_type = self.detect_document_type()
