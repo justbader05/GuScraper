b2939d90a11dbae10d1129032f94a3037434f815
Bader Al Refai <badermajidalrefai@gmail.com>
2026-09-20 05:52:20 +0300

    Added user initialization and default download_path

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
+from pathlib import Path
+
 from database.base import Base
 from database.engine import engine
+from sqlalchemy.orm import Session
+from sqlalchemy import select
 from scripts.scrape import scrape_files
+from scripts.organize import organize_files
+from classes.user import User
 
 
 if __name__ == "__main__":
     Base.metadata.create_all(engine)
-    scrape_files(1)
+    with Session(engine) as e:
+        user = e.get(User, 1)
+
+    download_path = Path(user.obsidian_root)
+    organize_files(scrape_files(1,download_path), download_path, engine) 
